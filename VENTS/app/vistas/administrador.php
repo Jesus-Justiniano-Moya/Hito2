783520a1967a5562a0c73dc294a23533cbdea246
Jesus-Justiniano-Moya <73489934+Jesus-Justiniano-Moya@users.noreply.github.com>
2021-06-18 19:46:43 -0500

    Add files via upload

---
+++ VENTS/app/vistas/administrador.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+				<ul class="menu">
+         <li><a href="/VENTS/signup.php">Crear usuario</a></li>
+         <li><a href="/VENTS/cre.php">Actualizar o Eliminar Usuario</a></li>
+         <li><a href="/VENTS/cre.php">Añanir Pedido</a></li>
+         <li><a href="/VENTS/cre.php">Actualizar o Eliminar Pedido</a></li>
+         
+         
+         
+        </ul>
+  </div>
+
+
+</body>
+</html>
+</br></br></br></br>
+
+</br></br></br></br>
